5c56cab20d1dbcd141999ba0f4bc30397230841c
Jose Rocabado <jose_1298@hotmail.com>
2022-03-04 20:21:25 +0100

    Commit con la practica dos con lo que se hizo en clase

--- Practica2/DP_MOD.v
+++ Practica2/DP_MOD.v
@@ -12,16 +12,40 @@ module DP_MOD
 	output val_out
 	);
 	
-	
-	
-	
- DDS #(.M(24),.L(15),.W(16)) D1
-		(
-		.P( ),
-		.rst_AC(  ), 
-		.en_AC( ),
-		.clk( ),
-		.sin_wave()
-		);
+	// Definition of auxiliar variables
+	reg signed [23:0] im_fm_mux_x_i_data; //Register 1
+	reg signed [23:0] frec_por_sum_res; //Register 2
+	reg signed [15:0] o_data_aux; //Register 3
+	reg signed [16:0] im_fm_mux; //Register 4 [a-:b] ->-:b = numero de bits a coger a partir de a 
+	reg rst_pipe1, rst_pipe2; //Register 5, 6
+	reg signed [16:0] am_mux; //register 7
+	reg signed [15:0] i_data_pipe1, i_data_pipe2,i_data_pipe3; //Register 8, 9, 10
+	reg signed [15:0] im_am_x_i_data_pipe_3; //Register 11
+	reg signed [16:0] uno_sum; //Register 12
+
+	// Defining AM/FM selector
+	always @(posedge clk ) 
+		if(c_fm_am)
+			begin
+				im_fm_mux <= im_fm; // ajustar a la 
+				am_mux <= 2'b01 << 15;
+			end
+		else
+			begin
+				im_fm_mux <= 1'b0;
+				am_mux <= uno_sum >> 1; //escalado -> división por 2
+			end
+
+	// Defining FM path
+	always @(posedge clk ) 
+		begin
+			im_fm_mux_x_i_data <= im_fm_mux * i_data ;
+		end
+
+ 	DDS #(.M(24),.L(15),.W(16)) D1 (.P( ),
+									.rst_AC(  ), 
+									.en_AC( ),
+									.clk( ),
+									.sin_wave());
 
 endmodule 
