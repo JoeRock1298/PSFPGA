9bba7f106322739253dec004dd4676bfdb4938aa
Jose rocabado <-v>
2022-04-06 20:02:52 +0200

    Comiit hasta el paso 4

--- Practica3/CIC_pc.v
+++ Practica3/CIC_pc.v
@@ -1,6 +1,7 @@
 module CIC_pc
 #(parameter Win=16, // Input length
-  parameter Wg=22)  // Guard bits
+  parameter Wg=22, // Guard bits
+  parameter N= 3)  // Número de bloques a implementar  (cuenta el 0)
 
   (input signed [Win-1:0] i_data,  		// Data input
    input clk,
@@ -12,48 +13,54 @@ module CIC_pc
 //// Insertar la descripción del modulo
 //// Modulo CIC_pc: Top del filtro interpolador CIC usando generate
 
-genvar i;
+wire val_wire [0:7];
+
+wire [Win+Wg-1:0] data_int_wire [0:3];
+
+wire [(Win-1)+N:0] data_comb_wire [0:3];
+
+genvar c;
 
 generate 
-
-	for(i=0; i < 3; i= i+1) 
-		begin 
-			wire val_wire [0:7];
-			wire [Win+Wg-1:0] data_in_int_wire [0:2];
-			wire [Win+Wg-1:0] data_out_int_wire [0:2];
-			wire [(Win-1)+i:0] data_comb_wire [0:2];
-			wire [(Win-1)+i:0] data_out_comb_wire [0:2];
+	begin
 //COMB
- 			COMB #(.Win(Win+i)) COMB (.data_in(data_in_comb_wire[i]),
-   									.clk(clk),
+	for(c=0; c < 3; c= c+1) 
+	begin : CIC
+ 			COMB #(.Win(Win + c)) COMB0 (.data_in(data_comb_wire[c]),
+											.clk(clk),
                						.rst(rst), 
-	 								.val_in(val_wire[i]),
-									.val_out(val_wire[i+1]),
-            			  			.data_out((data_out_comb_wire[i+1]));
-//INT
-			INT #(.Win(Win), .Wg(Wg)) INT (.data_in(data_in_int_wire[i]),
-   							        	.clk(clk),
-               	        				.rst(rst), 
-								        .val_in(val_wire[i+4]),
-								        .val_out(val_wire[i+5]),
-                       				 	.data_out(data_out_int_wire[i+1]));					
-// R_INT
-			if (i == 0)
-			begin 
- 				R_INT #(.Win(Win+3))  R_INT (.data_in(data_out_comb_wire[3]),
-   											.clk(clk),
-                	  						.rst(rst), 
-	 										.val_in(val_wire[3]),
-											.val_out(val_wire[4]),
-                   							.data_out(data_in_int_wire[0]));	
-			end								
-		end
+											.val_in(val_wire[c]),
+											.val_out(val_wire[c+1]),
+											.data_out(data_comb_wire[c+1]));
+
+
+//INT		
+
+			INT #(.Win(Win), .Wg(Wg)) INT0 (.data_in(data_int_wire[c]),
+													.clk(clk),
+													.rst(rst), 
+													.val_in(val_wire[c+4]),
+													.val_out(val_wire[c+5]),
+													.data_out(data_int_wire[c+1]));		
+		end			
+
+
+	end
 endgenerate
+
+// R_INT
+			R_INT #(.Win(Win+3))  R_INT0 (.data_in(data_comb_wire[3]),
+												.clk(clk),
+												.rst(rst), 
+												.val_in(val_wire[3]),
+												.val_out(val_wire[4]),
+												.data_out(data_int_wire[0]));			
+
 	
-assign 	val_in = val_wire[0];
+assign 	val_wire[0] = val_in ;
 assign 	val_out = val_wire[7];
-assign	data_in_wire = data_in_comb_wire [0];
-assign 	data_out_wire = data_out_int_wire [3];
+assign	data_comb_wire [0] = i_data;
+assign 	o_data = data_int_wire [3];
 
 endmodule 
 
