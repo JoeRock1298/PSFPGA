2127e3d8a2c34bc9602bada1fe2626769d632720
Gianmaco Sangoi <68665808+G-marco96@users.noreply.github.com>
2022-04-29 03:00:25 +0200

    Update SEC_FILER.v

--- Practica4/E4_1/SEC_FILER.v
+++ Practica4/E4_1/SEC_FILER.v
@@ -9,7 +9,35 @@ module SEC_FILTER
    output reg val_out,
    output reg signed [Win+Wc-1:0] dout); // salida precision completa
    //output reg signed [Win+2:0] dout); // salida truncada a 19 bits
+	
+  //Wires
 
+	//Modules
+	MULT_ACC #(.Win(Win), .Wc(Wc)) MULT_ACC0(.din(),
+					        .coef(),
+					        .clk(clk),
+					        .rst(),
+					        .ce(),
+					        .dout());
+		
+	REG_MUX #(.Win(Win), .Num_coef(Num_coef)) REG_MUX0(.din(),
+					     		   .sel(),
+					       		   .clk(),
+					      		   .ce(),
+					       		   .dout());
+				   
+	ROM #(.Win(Win), .Wc(WC)) ROM0(.addr(),
+				       .clk(),
+				       .data());
+						 
+	CONTROL #(.Num_coef(Num_coef)) CONTROL0(.val_in(),
+					        .val_out(),
+					        .clk(),
+					        .rst(),
+					        .ce(),
+					        .addr())
+						.ce_Reg(),
+		  				.rst_ACC(),
+		   				.ce_ACC());
 
-	 
 endmodule
